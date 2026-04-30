62972549297667676d3f4fc687bd589db28d7152
Kenny Pitt <kenny.pitt@gmail.com>
2026-04-30 17:43:23 -0400

    feat(cmd): Implement 'solve' command skeleton

---
+++ internal/cmd/solve.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var solveCmd = &cobra.Command{
+	Use:   "solve [puzzle]",
+	Short: "Solve a Sudoku puzzle",
+	Long:  `Solve a Sudoku puzzle provided as an 81-character string.`,
+	Args:  cobra.MaximumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			fmt.Println("Please provide a Sudoku puzzle string.")
+			return
+		}
+		puzzle := args[0]
+		fmt.Printf("Attempting to solve puzzle: %s\n", puzzle)
+		// Logic will be added in later tasks
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(solveCmd)
+}
